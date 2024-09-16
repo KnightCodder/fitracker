1628ff487c6fdc79717a16c425ee419a45b03f13
KnightCodder <knightcodder@gmail.com>
2024-09-16 15:42:24 +0530

    fixed some bugs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,11 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.KnightCodder.fitracker',
   appName: 'fitracker',
-  webDir: 'public'
+  server: {
+    url: 'https://fitracker-five.vercel.app/'
+  }
 };
 
 export default config;
